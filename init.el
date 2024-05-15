7f9fc1eac811795fa56794bd996d86e5117dc1f4
Шакиров Артём <artdmshakirov@gmail.com>
2024-05-15 17:32:23 +0400

    Modules: helm, projectile, no new tabs

--- init.el
+++ init.el
@@ -4,9 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(wombat))
- '(display-buffer-base-action '(display-buffer-in-new-tab))
  '(ispell-dictionary nil)
- '(package-selected-packages '(magit company dashboard))
+ '(package-selected-packages '(helm-projectile projectile helm magit company dashboard))
  '(tab-bar-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -29,9 +28,18 @@
 (dashboard-setup-startup-hook)
 
 (setq tab-bar-new-tab-choice "*dashboard*")
+(setq native-comp-async-report-warnings-errors 'silent)
 
 (defun close-tab-and-buffer()
   (interactive)
   (kill-buffer)
   (tab-close))
 (keymap-global-set "C-x t w" 'close-tab-and-buffer)
+
+(helm-mode 1)
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
