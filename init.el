b874d6ccab6731dbd2beb389e6a42812fa43bf99
Шакиров Артём <artdmshakirov@gmail.com>
2024-10-10 15:47:11 +0400

    Clangd, stm32

--- init.el
+++ init.el
@@ -4,9 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(wombat))
+ '(gdb-debuginfod-enable-setting t)
  '(ispell-dictionary nil)
  '(package-selected-packages '(helm-projectile projectile helm magit company dashboard))
  '(tab-bar-mode t)
+ '(package-vc-selected-packages
+   '((clangd-inactive-regions :vc-backend Git :url "https://github.com/fargiolas/clangd-inactive-regions.el")))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -22,6 +25,7 @@
              '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
 (add-hook 'python-mode-hook 'eglot-ensure)
+(add-hook 'c-mode-hook 'eglot-ensure)
 (add-hook 'after-init-hook 'global-company-mode)
 
 (setq dashboard-projects-backend 'projectile)
@@ -32,16 +36,14 @@
                         (registers . 5)))
 (require 'dashboard)
 (dashboard-setup-startup-hook)
-
-
 (setq tab-bar-new-tab-choice "*dashboard*")
 (setq native-comp-async-report-warnings-errors 'silent)
 
-(defun close-tab-and-buffer()
-  (interactive)
-  (kill-buffer)
-  (tab-close))
-(keymap-global-set "C-x t w" 'close-tab-and-buffer)
+;; (defun close-tab-and-buffer()
+;;   (interactive)
+;;   (kill-buffer)
+;;   (tab-close))
+;; (keymap-global-set "C-x t w" 'close-tab-and-buffer)
 
 (helm-mode 1)
 (global-set-key (kbd "M-x") #'helm-M-x)
@@ -50,3 +52,24 @@
 
 (projectile-mode +1)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+(speedbar 1)
+
+(add-to-list 'load-path "~/.emacs.d/stm32")
+(require 'stm32)
+
+(global-auto-revert-mode t)
+
+(unless (package-installed-p 'clangd-inactive-regions)
+  (package-vc-install "https://github.com/fargiolas/clangd-inactive-regions.el"))
+
+(use-package clangd-inactive-regions
+  :init
+  (add-hook 'eglot-managed-mode-hook #'clangd-inactive-regions-mode)
+  :config
+  (clangd-inactive-regions-set-method "darken-foreground")
+  (clangd-inactive-regions-set-opacity 0.55))
+(put 'dired-find-alternate-file 'disabled nil)
+
+(global-set-key (kbd "<C-tab>") 'previous-buffer)
+(global-set-key (kbd "<C-S-iso-lefttab>") 'next-buffer)
